8be9871c726e8ab57a4cc1b50969aa1eb6b6f1ab
xsam951 <tadrissi.samuel@gmail.com>
2024-06-12 14:27:30 +0200

    changing files

--- vpc.tf
+++ vpc.tf
@@ -1,103 +1,18 @@
-# Create a VPC to launch our instances into
-resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"  
-  enable_dns_hostnames = true 
-  enable_dns_support = true
-  
-  tags       =  {
-    Name     = "deham14-vpc"
-  }       
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_subnet" "public-1" {
-  vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-  map_public_ip_on_launch = true
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+#  enable_nat_gateway = false
+#  enable_vpn_gateway = false
 
   tags = {
-    Name = "deham14-public-subnet-1"
+    Terraform = "true"
+    Environment = "dev"
   }
-}
-
-resource "aws_subnet" "private-1" {
-  vpc_id     = aws_vpc.dev_vpc.id 
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "deham14-private-subnet-1" 
-  }
-}
-
-resource "aws_subnet" "public-2" {
-  vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "us-west-2b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "deham14-public-subnet-2"
-  }
-}
-
-resource "aws_subnet" "private-2" {
-  vpc_id     = aws_vpc.dev_vpc.id 
-  cidr_block = "10.0.4.0/24"
-  availability_zone = "us-west-2b"
-
-  tags = {
-    Name = "deham14-private-subnet-2" 
-  }
-}
-
-# Create an Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.dev_vpc.id
-  tags = {
-    Name = "deham14-IGW"
-  }
-}
-
-resource "aws_route_table" "RB_Public_RouteTable" {
-  vpc_id = aws_vpc.dev_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  tags = {
-    Name = "deham14-public-route-table"
-  }
-}
-
-resource "aws_route_table" "RB_Private_RouteTable" {
-  vpc_id = aws_vpc.dev_vpc.id
-  tags = {
-    Name = "deham14-private-route-table"
-  }
-}
-
-resource "aws_route_table_association" "Public_Subnet1_Asso" {
-  route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-1.id
-  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
-}
-
-resource "aws_route_table_association" "Private_Subnet1_Asso" {
-  route_table_id = aws_route_table.RB_Private_RouteTable.id
-  subnet_id      = aws_subnet.private-1.id
-  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
-}
-
-resource "aws_route_table_association" "Public_Subnet2_Asso" {
-  route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-2.id
-  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
-}
-
-resource "aws_route_table_association" "Private_Subnet2_Asso" {
-  route_table_id = aws_route_table.RB_Private_RouteTable.id
-  subnet_id      = aws_subnet.private-2.id
-  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
 }
